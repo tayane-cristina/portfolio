7a6341775430002fc1e6e0c98f32380473ef54b6
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-09-11 15:46:36 -0300

    font-ajusted

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <li className='li-about-img'><img className='img-me' src="https://img.freepik.com/fotos-premium/retrato-da-vista-lateral-de-uma-desenvolvedora-de-ti-digitando-no-teclado-com-codigo-de-programacao-preto-e-laranja-na-tela-do-computador-e-laptop-no-interior-contemporaneo-do-escritorio-copie-o-espaco_236854-24288.jpg?size=626&ext=jpg&ga=GA1.1.2089675641.1676983403&semt=sph" alt="foto-minha"></img></li>
         <li className='li-about-text'>
             <h2 className="h2-about-me">About me</h2>
-            <p>Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet
+            <p className="p-about">Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet
                 Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set ametLorem ipsum set amet Lorem ipsum set amet
                 Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set amet<br /><br />
                 Lorem ipsum set amet Lorem ipsum set amet Lorem ipsum set ametLorem ipsum set ametLorem ipsum set amet 
