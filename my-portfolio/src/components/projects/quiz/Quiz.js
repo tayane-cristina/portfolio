829dd78bb416db7a17a09158ff79a6562bd1dc28
Tayane Cristina <tay-cristinarj@hotmail.com>
2024-06-13 19:07:44 -0300

    test...

--- my-portfolio/src/components/projects/quiz/Quiz.js
+++ my-portfolio/src/components/projects/quiz/Quiz.js
@@ -3,26 +3,31 @@ import data from './Data'
 
 const  Quiz = () => {
 
-    //The ideal way to print in the project in the chomer is, take a 
+    //The ideal way to print the project in the chomer is, take the states
+    //of the question, the answer and the options, give their a space on the return and 
+    // use a function to give the states the values who needs to apper in the screen, on the correct space
+
+    //Maybe will be necessare create a function to decide what is the question who will be map in the function of show question
 
     const [answer, setAnswer] = useState("");
-    const [question, setQuestion] = useState("");
+    const [currentQuestion, setCurrentQuestion] = useState();
     const [options, setOptions] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0)
 
     //Func to map the question and the answer, and edit the state of the objcts
-
+    const showQuestion = () => {
+        const ask = data[currentIndex].question
+    }
 
     console.log(data.map((ask) => (ask.question)))
     console.log(data.map((ask) => (ask.options[0])))
   return (
     <div className='principal-div div-quiz'>
       <h1>Quiz</h1>
-      {data.map((ask, index) => (
-        <ul key={index}>
-            <h2>{ask.question}</h2>
-            {ask.options.map((option, i) => <li key={i}>{option.opt}</li>)}
+      <h2>Pergunta: {data[currentIndex].question}</h2>
+        <ul>
+            {data.map((ask, index) => (ask[currentIndex].options.map((option) => console.log(option))))}
         </ul>
-      ))}
     </div>
   );
 };
