0f4211cac4083c0cd42b9e6123b6f7e368ec0cce
Tayane Cristina <tay-cristinarj@hotmail.com>
2024-06-13 17:50:38 -0300

    think about the logic quiz

--- my-portfolio/src/components/projects/quiz/Quiz.js
+++ my-portfolio/src/components/projects/quiz/Quiz.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './Data'
 
 const  Quiz = () => {
+
+    //The ideal way to print in the project in the chomer is, take a 
+
+    const [answer, setAnswer] = useState("");
+    const [question, setQuestion] = useState("");
+    const [options, setOptions] = useState([]);
+
+    //Func to map the question and the answer, and edit the state of the objcts
+
 
     console.log(data.map((ask) => (ask.question)))
     console.log(data.map((ask) => (ask.options[0])))
@@ -9,10 +18,10 @@ const  Quiz = () => {
     <div className='principal-div div-quiz'>
       <h1>Quiz</h1>
       {data.map((ask, index) => (
-        <li key={index}>
+        <ul key={index}>
             <h2>{ask.question}</h2>
             {ask.options.map((option, i) => <li key={i}>{option.opt}</li>)}
-        </li>
+        </ul>
       ))}
     </div>
   );
